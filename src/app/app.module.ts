bd600decb726bd361dcf3fb77789d607922af91d
Flipize <nilsson.filip@hotmail.se>
2019-03-07 14:18:43 +0100

    Ny titel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AddComponent } from './add/add.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    AddComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule
