c5409cd4ffc24f209c9edc03e49661d7306ae897
Flipize <nilsson.filip@hotmail.se>
2019-03-08 00:11:17 +0100

    Yeye

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ToolbarComponent} from './toolbar/toolbar.component';
+import {MoviesComponent} from './component/movies/movies.component';
+import {AddComponent} from './component/add/add.component';
+import {LogInComponent} from './component/log-in/log-in.component';
+import {HomeComponent} from './component/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: ToolbarComponent}
+  {path: '', component: HomeComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: 'add', component: AddComponent},
+  {path: 'login', component: LogInComponent}
 ];
 
 @NgModule({
