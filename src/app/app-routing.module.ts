8ff56a4b6bb0359ca12c909c4fe478da22e32be5
beigart <beigart94@gmail.com>
2019-03-08 10:18:23 +0100

    Tjänna

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,24 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import {Routes, RouterModule} from '@angular/router';
-import {MoviesComponent} from './movies/movies.component';
-import {AddComponent} from './add/add.component';
-import {LogInComponent} from './log-in/log-in.component';
-import {ToolbarComponent} from './toolbar/toolbar.component';
+import {MoviesComponent} from './component/movies/movies.component';
+import {AddComponent} from './component/add/add.component';
+import {LogInComponent} from './component/log-in/log-in.component';
+import {ToolbarComponent} from './component/toolbar/toolbar.component';
+import {HomeComponent} from './component/home/home.component';
 
 const routes: Routes = [
   {path: 'toolbar', component: ToolbarComponent},
   {path: 'movies', component: MoviesComponent},
   {path: 'add', component: AddComponent},
   {path: 'log-in', component: LogInComponent},
+  {path: '', component: HomeComponent}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-=======
-import {RouterModule, Routes} from '@angular/router';
-import {MoviesComponent} from './component/movies/movies.component';
-import {AddComponent} from './component/add/add.component';
-import {LogInComponent} from './component/log-in/log-in.component';
-import {HomeComponent} from './component/home/home.component';
-
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'movies', component: MoviesComponent},
-  {path: 'add', component: AddComponent},
-  {path: 'login', component: LogInComponent}
-];
 
 @NgModule({
   exports: [RouterModule],
   imports: [
     RouterModule.forRoot(routes)
   ]
->>>>>>> origin/master
 })
 export class AppRoutingModule { }
