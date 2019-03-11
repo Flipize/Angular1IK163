24663b118ea8251f07dfdacb5905e0567f5dbd79
LitenSnigel <43867457+LitenSnigel@users.noreply.github.com>
2019-03-11 12:02:02 +0100

    404 gif + component

---
+++ src/app/component/not-found/not-found-component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-component',
+  templateUrl: './not-found-componen.html',
+  styleUrls: ['./not-found-component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
