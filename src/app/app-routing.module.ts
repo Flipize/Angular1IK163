e37f292339f4828a91a7f0f84589496ee996db77
LitenSnigel <43867457+LitenSnigel@users.noreply.github.com>
2019-03-11 12:09:57 +0100

    404 gif + component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import {LogInComponent} from './component/log-in/log-in.component';
 import {ToolbarComponent} from './component/toolbar/toolbar.component';
 import {HomeComponent} from './component/home/home.component';
 import { NotFoundComponent } from './component/not-found/not-found-component';
+import { AuthGuardService} from './services/auth-guard.service';
 
 const routes: Routes = [
   {path: 'toolbar', component: ToolbarComponent},
   {path: 'movies', component: MoviesComponent},
-  {path: 'add', component: AddComponent},
+  {path: 'add', component: AddComponent, canActivate: [AuthGuardService]},
   {path: 'login', component: LogInComponent},
   {path: '', component: HomeComponent},
   {path: '**', component: NotFoundComponent}
