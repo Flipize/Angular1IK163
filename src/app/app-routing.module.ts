f90dcb83128666d78683958b27da9a36992401a0
Flipize <nilsson.filip@hotmail.se>
2019-03-11 11:59:17 +0100

    Yeye

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import {AddComponent} from './component/add/add.component';
 import {LogInComponent} from './component/log-in/log-in.component';
 import {ToolbarComponent} from './component/toolbar/toolbar.component';
 import {HomeComponent} from './component/home/home.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   {path: 'toolbar', component: ToolbarComponent},
   {path: 'movies', component: MoviesComponent},
-  {path: 'add', component: AddComponent},
+  {path: 'add', component: AddComponent, canActivate: [AuthGuardService]},
   {path: 'login', component: LogInComponent},
   {path: '', component: HomeComponent},
 ];
